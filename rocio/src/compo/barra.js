0cdcc8e757da903180e75b300891fd4f356ae992
draco <draco25vs@gmail.com>
2023-04-11 11:32:23 +0200

    vesion 1.0

---
+++ rocio/src/compo/barra.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Barra = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/" class>Inicio</a>
+        </li>
+        <li>
+          <a href="/exposiciones">Exposiciones</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+        <li>
+          <a href="/galeria">Galería</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
---
+++ rocio/src/compo/cuerpo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Cuerpo = () => {
+  return(
+    <h1>Cuerpo</h1>
+  )
+}

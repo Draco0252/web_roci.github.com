d57fb6716c01b99c7a6c36eea7b9c4831287dafb
draco <draco25vs@gmail.com>
2023-04-13 18:45:49 +0200

    Ahora SI

--- rocio/src/compo/inicio.js
+++ rocio/src/compo/inicio.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-function Inicio() {
+function Inicio () {
   return (
     <div>
-      <h1>Página de Inicio</h1>
-      <p>Bienvenido a nuestra página principal.</p>
+     	<section>
+ 	  		<h1>Inicio</h1>
+ 	  		<p>Bienvenidos a nuestra tienda en línea.</p>
+ 	  	</section>
+ 	  	<section>
+ 	  		<h1>Exposiciones</h1>
+ 	  		<p>Aquí encontrarás información sobre nuestras exposiciones.</p>
+ 	  	</section>
+ 	  	<section>
+ 	  		<h1>Blog</h1>
+ 	  		<p>Lee nuestros artículos y descubre más sobre nuestras noticias.</p>
+ 	  	</section>
+ 	  	<section>
+ 	  		<h1>Venta</h1>
+ 	  		<p>Explora nuestra selección de productos y haz tu compra.</p>
+   		</section>
     </div>
   );
 }

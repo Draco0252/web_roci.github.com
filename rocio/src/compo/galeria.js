b0f787982201ac5752dfe721cea0ff0f87111f00
draco <draco25vs@gmail.com>
2023-04-20 17:36:05 +0200

    primera suvida de bakup

--- rocio/src/compo/galeria.js
+++ rocio/src/compo/galeria.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import IMG from './img/favicon.png'
 
 function Galeria() {
   return (
     <div>
       <h1>Página de Galeria</h1>
       <p>Aqui te muestro mis cuadros.</p>
+      <div class='galeria'>
+        <div class='cuadros'>
+         <img src={IMG} />
+         <div>
+           <h3>Titulo</h3>
+           <p>Descripcion corta</p>
+           <p>precio</p>
+           <a href='#' class='boton'>Ver mas</a>
+         </div>
+       </div>
+         <div class='cuadros'>
+         <img src={IMG} />
+         <div>
+           <h3>Titulo</h3>
+           <p>Descripcion corta</p>
+           <p>precio</p>
+           <a href='#' class='boton'>Ver mas</a>
+         </div>
+        </div>
+      </div>
     </div>
   );
 }
--- rocio/src/compo/inicio.js
+++ rocio/src/compo/inicio.js
@@ -1,24 +1,19 @@
 import React from 'react';
+import IMG from "./img/ech.jpg"
+
 
 function Inicio () {
   return (
     <div>
-     	<section>
- 	  		<h1>Inicio</h1>
- 	  		<p>Bienvenidos a nuestra tienda en línea.</p>
- 	  	</section>
- 	  	<section>
- 	  		<h1>Exposiciones</h1>
- 	  		<p>Aquí encontrarás información sobre nuestras exposiciones.</p>
- 	  	</section>
- 	  	<section>
- 	  		<h1>Blog</h1>
- 	  		<p>Lee nuestros artículos y descubre más sobre nuestras noticias.</p>
- 	  	</section>
- 	  	<section>
- 	  		<h1>Venta</h1>
- 	  		<p>Explora nuestra selección de productos y haz tu compra.</p>
-   		</section>
+      <section class="sec">
+        <div class="text_R">
+          <h2>Título del texto</h2>
+          <p>Este es un párrafo de texto.</p>
+        </div>
+        <div class="image_R">
+          <img src={IMG} alt="Un cuadro apstrapto" />
+        </div>
+      </section>
     </div>
   );
 }

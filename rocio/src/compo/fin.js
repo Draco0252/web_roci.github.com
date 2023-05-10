99edbf653dc8a26496b8944f8007896b7e3eb694
draco <draco25vs@gmail.com>
2023-05-10 11:55:36 +0200

    Las fotos ya estan recortadas

--- rocio/src/compo/fin.js
+++ rocio/src/compo/fin.js
@@ -4,8 +4,7 @@ function Fin () {
   return(
     <div className="fin">
       <h2>Haqui te dejo mis redis sociales</h2>
-      <p>roci@algo.com</p>
-      <p>#arte_roci</p>
+      <p>Instagram : rocioburgosvargas</p>
     </div>
   );
 }
